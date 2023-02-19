6890d2e402d0ac55fcb3b1b104f9560db3bfdf7c
赵天峰 <zhaotianfeng@zhaotianfengdeMacBook-Pro.local>
2023-02-19 17:13:54 +0000

    touch screen drag test

---
+++ Assets/Scripts/Player/ScreenDrag.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenDrag : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.touchCount);
+        if (1 == Input.touchCount)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 deltaPos = touch.deltaPosition;
+            transform.Rotate(Vector3.down * deltaPos.x, Space.World);//绕Y轴进行旋转
+        }
+    }
+}
